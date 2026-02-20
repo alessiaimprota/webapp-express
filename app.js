acc6bef8993161a4ec2ca510b03f3bb1676d0971
alessiaimprota <alessiaimprota126@gmail.com>
2026-02-20 18:13:37 +0100

    metto tutto in app poi sistemooo

--- app.js
+++ app.js
@@ -2,10 +2,22 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+//IMPORTO un attimo qui per provare la rotta di index
+const connection = require("./data/db");
+
 app.get("/", (req, res) => {
-  res.send(console.log("ciao"));
+  res.send("ciaone");
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+});
+
+app.get("/movies", (req, res) => {
+  const sql = "SELECT * FROM movies";
+  //proviamo qua prima se funziona la rotta index
+  connection.query(sql, (err, results) => {
+    if (err) return res.status(500).json({ error: "Database query failed" });
+    res.json(results);
+  });
 });
