e1af25654c76c082ee781a2f93be29633eadf8e3
alessiaimprota <alessiaimprota126@gmail.com>
2026-02-24 17:24:27 +0100

    damn cors

--- app.js
+++ app.js
@@ -2,12 +2,16 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-
+//poliziaaa cors!!!
+const cors = require("cors");
 const port = process.env.PORT;
+//middlewares
 const notFound = require("./middlewares/notFound");
 const errorsHandler = require("./middlewares/errorsHandler");
 //importo i routes
 const moviesRoutes = require("./routes/routesMovies");
+
+app.use(cors({ origin: "http://localhost:5174" }));
 app.use(express.json());
 app.use(express.static("public"));
 //Gestisco da qua le routes dei movies così posso gestire le rotte dal file routes
@@ -25,13 +29,3 @@ app.listen(port, () => {
 
 app.use(errorsHandler);
 app.use(notFound);
-/*
-app.get("/movies/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const sql = "SELECT * FROM movies WHERE id = ?";
-  connection.query(sql, [id], (err, results) => {
-    if (err) return res.status(500).json({ error: "Database query failed" });
-    res.json(results[0]);
-  });
-});
-*/
